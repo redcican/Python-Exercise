dc4b9d4bb00728a9b69dcf3024f8bec439254a40
redcican <cican17@gmail.com>
2019-04-11 09:39:56 +0200

    Add a efficent way to python file.

--- 02_longest_word.py
+++ 02_longest_word.py
@@ -1,15 +1,21 @@
 # Find the longest word in a sentence
 
 def longestWord(sentence):
-    words = sentence.split(" ");
-    longestWord = "";
+    words = sentence.split(" ")
+    longestWord = ""
     for word in words:
         if len(word) > len(longestWord):
             longestWord = word
 
     return longestWord
 
-
+def efficentLongestWord(sentence):
+    words = sentence.split(" ")
+    longestWord = max(words, key=len)
+    return longestWord
 
 print(longestWord("I woke up early today"))
 print(longestWord("I went straight to the beach"))
+print("#####################################")
+print(efficentLongestWord("I woke up early today"))
+print(efficentLongestWord("I went straight to the beach"))
