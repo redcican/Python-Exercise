8d51960ae72ecb0b5ede47e1b094b639c49741e3
redcican <cican17@gmail.com>
2019-04-14 10:47:06 +0200

    implement two ways to capitalize the first letter of a sentence.

---
+++ 05_capitalize_first_letter.js
@@ -0,0 +1,27 @@
+// create a function to capitalize first letter
+
+function capitalizeWords(str){
+    let words = str.split(" ").map(word =>{
+        let firstLetter = word.slice(0,1);
+        let restLetter = word.slice(1);
+        firstLetter = firstLetter.toUpperCase();
+        return `${firstLetter}${restLetter}`;
+    });
+
+    return words.join(" ");
+}
+
+
+console.log(capitalizeWords("I got up early today"));
+console.log(capitalizeWords("I walked straight to the beach"));
+console.log("###############################")
+
+// a effective way
+function effectiveCapitalizeWords(str){
+    let words = str.split(" ").map(word=>{
+        return word.charAt(0).toUpperCase()+ word.slice(1);
+    });
+    return words.join(" ");
+}
+console.log(capitalizeWords("I got up early today"));
+console.log(capitalizeWords("I walked straight to the beach"));
