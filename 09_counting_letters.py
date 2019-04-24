999bac2bdb4cad0eb2d3aca88bd7792c77e33bea
redcican <cican17@gmail.com>
2019-04-24 11:08:36 +0200

    implement the python code.

---
+++ 09_counting_letters.py
@@ -0,0 +1,23 @@
+# return first word with greatest number of repeated letters
+from collections import Counter
+
+def countLetters(str):
+    tempArr = str.split(" ")
+    tempItem = [item.lower().split() for item in tempArr]
+    
+    amount = 1
+    letter = None
+
+    for character in tempItem:
+        for c in character:
+            counter = Counter(x for x in c).most_common(1)
+            count = counter[0][1]
+            if count > amount:
+                amount = count
+                letter = character
+    return letter
+
+
+
+
+print(countLetters("Javascript is the greatest programming language"))
